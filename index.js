50e06da5a3e63f5adc911a5b78789f423f191459
joshua28082000 <120441971+joshua28082000@users.noreply.github.com>
2023-01-04 12:03:47 +0530

    add a score tracker via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let score = document.getElementById("bts")
+ let count = 0
+function increase1(){
+    count +=1
+    score.innerText = count
+}
+function increase2(){
+    count +=2
+    score.innerText = count
+}
+function increase3(){
+    count +=3
+    score.innerText = count
+}
+ let score1 = document.getElementById("btss")
+ let count1 =0
+ function increase01(){
+    count1 +=1
+    score1.innerText = count1
+}
+function increase02(){
+    count1 +=2
+    score1.innerText = count1
+}
+function increase03(){
+    count1 +=3
+    score1.innerText = count1
+}
